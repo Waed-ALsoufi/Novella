055b1739f6cd2bed76fa85143adc3d48db91b589
Waed-ALsoufi <testcode481@gmail.com>
2021-04-29 13:11:47 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+  {
+    title: "Profile",
+    path: "/Profile",
+    cName: "dropdown-link",
+  },
+  {
+    title: "log out",
+    path: "/Logout",
+    cName: "dropdown-link",
+  },
+];
