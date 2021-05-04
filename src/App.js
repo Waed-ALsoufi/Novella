e7e17268902ff62e3c4dd83fddae4e58b6bd844a
Waed-ALsoufi <testcode481@gmail.com>
2021-05-05 01:14:20 +0300

    Add Routes File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import Home from "./components/Home";
-import Posts from "./components/Postrender";
-import Publish from "./components/Publish";
-import Details from "./components/Details";
+import Routes from "./components/NavRoutes";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/Posts" component={Posts} />
-        <Route path="/Publish" component={Publish} />
-        <Route path="/Details" component={Details} />
-      </Switch>
+      <Routes />
     </Router>
   );
 }
