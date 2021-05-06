4ff9933eb8936874791da8f572ac11bc738ee57d
Waed-ALsoufi <testcode481@gmail.com>
2021-05-06 13:05:00 +0300

    edit slider

--- src/components/imageSlider.js
+++ src/components/imageSlider.js
@@ -1,10 +1,12 @@
-import books1 from "../Images/books1.jpg";
+import books1 from "../Images/silderbg2.jpeg";
+import books2 from "../Images/sliderbg.jpeg";
 
 const imageSlider = [
   {
-    title: "First image slide",
-    description: " This the first image slide",
-    urls: books1,
+    title: "Welcome To Website Name",
+    description:
+      " Website Name is an online community that allows readers and book owners to exchange books.",
+    urls: books2,
   },
   {
     title: "Second image slide",
