2255280f74b3e3b80d76bacb7b308025cf2a4cac
Waed-ALsoufi <testcode481@gmail.com>
2021-05-08 16:48:18 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Main from "./components/Main";
-import Slider from "./components/Slider";
+// import Slider from "./components/Slider";
 function App() {
-  const showLaning = false;
-  return <div>{showLaning ? <Slider /> : <Main />}</div>;
+  //   const showLaning = false;
+  return <div>{<Main />}</div>;
 }
 
 export default App;
