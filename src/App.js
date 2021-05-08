16527c99aeb49ea7a210ab410d10372b7d8caf45
Waed-ALsoufi <80908067+Waed-ALsoufi@users.noreply.github.com>
2021-05-08 16:56:11 +0300

    Revert "Add Routes File"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import Routes from "./components/NavRoutes";
+import Home from "./components/Home";
+import Posts from "./components/Postrender";
+import Publish from "./components/Publish";
+import Details from "./components/Details";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/Posts" component={Posts} />
+        <Route path="/Publish" component={Publish} />
+        <Route path="/Details" component={Details} />
+      </Switch>
     </Router>
   );
 }
