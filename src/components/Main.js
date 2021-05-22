21e07d86beb29ae1cc772c62dc3da38178a22a47
Waed-ALsoufi <testcode481@gmail.com>
2021-05-22 09:40:36 +0300

    Private Route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "../App.css";
-import Routes from "./NavRoutes";
-
+import Login from "./LogIn";
+import SignUp from "./SignUp";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import Routes from "./NavRoutes";
+import { AuthProvider } from "./Auth";
+
 function Main() {
   return (
     <Router>
-      <Navbar />
-      <Routes />
+      <AuthProvider>
+        <Switch>
+          <React.Fragment>
+            <div>
+              <Route path="/Login" component={Login} />
+              <Route path="/SignUp" component={SignUp} />
+              <Route path="/Logout" component={Login} />
+            </div>
+            <div>
+              <Navbar />
+              <Routes />
+            </div>
+          </React.Fragment>
+        </Switch>
+      </AuthProvider>
     </Router>
   );
 }
