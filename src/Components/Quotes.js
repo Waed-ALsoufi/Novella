3bdba24155997bee6ed69dc3f765d56b64f67951
6bisanraed <bisanraed7@gmail.com>
2021-06-18 12:03:21 +0300

    Edit Quotes

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 const Quotes = () => {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
+  const [count, setCount] = useState(0);
   useEffect(() => {
     let active = true;
 
@@ -26,22 +27,10 @@ const Quotes = () => {
     return () => {
       active = false;
     };
-  }, []);
-  const getQuotes = () => {
-    const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+  }, [count]);
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        const dataQuotes = data.quotes;
-        const randomNum = Math.floor(Math.random() * dataQuotes.length);
-        const randomQuote = dataQuotes[randomNum];
-        setQuote(randomQuote.quote);
-        setAuthor(randomQuote.author);
-      });
-  };
   const handleClick = () => {
-    getQuotes();
+    setCount(count + 1);
   };
 
   return (
